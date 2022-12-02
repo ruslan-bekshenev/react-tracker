e9cf8c005744f54abe38f37d6f8be169e3f14f8b
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-12-02 20:19:19 +0100

    timer component

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Clock = () => {
+  return (
+    <div>
+      asd
+    </div>
+  );
+};
+
+export default Clock;
--- src/pages/Timer/index.tsx
+++ src/pages/Timer/index.tsx
@@ -1,7 +1,18 @@
 import React from "react"
+import Clock from "../../components/Clock"
+import Tasks from "../../components/Tasks"
 
 const Timer = () => {
-  return <div>321312321</div>
+  return (
+    <div>
+      <div className="clock">
+        <Clock />
+      </div>
+      <div className="task-container">
+        <Tasks />
+      </div>
+    </div>
+  )
 }
 
 export default Timer
