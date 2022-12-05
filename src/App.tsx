f5bb38f3de93b4d1804fa563038eda8d35f5a6bc
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-12-05 22:25:07 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
+import Timer from "./pages/Timer"
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<div>123</div>}></Route>
+          <Route path="/" element={<Timer />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Timer/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Timer = () => {
+  return <div>asd</div>
+}
+
+export default Timer
